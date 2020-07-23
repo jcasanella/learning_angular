b461097f87243c1c82916fe0d201286c784e4979
Jordi Casanella <jordikiter@hotmail.es>
2020-07-23 08:05:54 +0100

    Router to hero page

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../service/heroes.service';
 import { Heroe } from '../../model/hero.interface';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -11,13 +11,19 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor( private _heroesService: HeroesService ) {
-    console.log("This is the constructor");
+  constructor( private _heroesService: HeroesService,
+              private _router: Router ) {
+    // console.log("This is the constructor");
   }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+
+  verHeroe( idx:number ) {
+    console.log(idx);
+    this._router.navigate( ['/heroe', idx] );
   }
 
 }
